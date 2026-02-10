ee57b7db6e21ee90a1ff7ec2b42fe9bb1a0d5419
KakeguriMashoBayou <boyul@nycstudents.net>
2026-02-10 09:00:06 -0500

    Update GCF.py

--- GCF.py
+++ GCF.py
@@ -1,6 +1,10 @@
 one = int(input("Put a number:  "))
-two = int(input("Put second number:  "))
+two = int(input("Put a number:  "))
 print("factors of", one and two)
-for i in range(two, one+1):
-    if one + two%i == 0:
-        print (i)
+for i in range(1, one+1):
+    if i <= two: 
+        if one%i == 0 and two%i == 0:
+            gcf = i
+            
+print ("The GCF is:", gcf)
+
